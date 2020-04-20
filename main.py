1e637f4ca51747207b58ed75301a9aebd055c589
sjLee <lsjj096@gmail.com>
2020-04-20 14:55:25 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import argparse
 from torch import optim
-from generator import Generator
-from discriminator import Discriminator
+from models.generator import Generator
+from models.discriminator import Discriminator
 import config
 
 def get_args():
@@ -17,7 +17,7 @@ def get_args():
 
     parser =argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description= "Pytorch implementation of 'Unsupervised Anomaly Detection with Generative Adversarial Networks to Guide Marker Discovery'"
+        description= "Pytorch implementation of 'Unsupervised Anomaly Detection with Generative Adversarial Networks to Guide Marker Discovery'")
 
     parser.add_argument('--pretrained', dest='pretrained', help="switch for using pretrained model",
                         action='store_true', default=False)
@@ -40,10 +40,55 @@ def get_args():
 def main():
     args = get_args()
 
-
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     transform = transforms.Compose([
         transforms.Scale()
     ])
 
-    G = Generator()
-    D = Discriminator()
+
+    transform = transforms.Compose([
+        transforms.Scale((512,512,3)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean = (0.5,0.5,0.5),
+                             std =  (0.5,0.5,0.5))
+    ])
+
+    G = Generator().to(device)
+    D = Discriminator().to(device)
+
+    loss_func = nn.MSELoss()
+
+    G_optim = torch.optim.adam(G.parameters(), lr = 0.01)
+    D_optim = torch.optim.adam(D.parameters(), lr = 0.01)
+
+    epochs = 500
+    import tqdm
+    for epoch in tqdm(range(epochs)):
+        for batch_idx, (x, labels) in tqdm(enumerate(train_loader)):
+            # discriminator
+
+            z = init.normal(torch.Tensor(batch_size, 100), mean=0, std=0.1).to(device)
+            D_z, _ = D(G(z))
+            D_x, _ = D(x)
+
+            zeros = torch.zeros(batch_size,1).to(device)
+            D_loss = torch.sum(loss_func(D_z, zeros)) + torch.sum(loss_func(D_imgs,ones_label))
+
+            D_optim.zero_grad()
+            D_loss.backward()
+            D_optim.step()
+
+            #generator
+
+            z = init.normal(torch.Tensor(batch_size , 100), mean = 0 , std = 0.1 ).to(device)
+            G_z = G(z)
+            D_z = D(G_z)
+
+            ones = torch.ones(batch_size,1)
+            G_loss = torch.sum(loss_func(D_z,ones))
+            G_optim.zero_grad()
+            G_loss.backward(retain_graph = True)
+            G_optim.step()
+
+            # model save
+
--- utils/utils.py
+++ utils/utils.py
@@ -9,22 +9,43 @@ import torchvision.transforms as transforms
 import argparse
 from torch import optim
 
-def anomaly_score(G,D, ano_z, test_input):
-    """
-    Calculate anomaly score
-    :param G:
-    :param D:
-    :param ano_z:
-    :param test_input:
-    :return:
-    """
-    ano_G = G(ano_z)
-    residual_loss = torch.sum(torch.abs(test_input - ano_G))
 
-    feature_ano_G , _ = D(ano_G)
-    feature_input , _ = D(test_input)
-    discriminator_loss = torch.sum(torch.abs(feature_ano_G - feature_input))
+class AverageMeter():
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
 
-    total_loss = (1 - ano_coeff) * residual_loss + ano_coeff * discriminator_loss
-    return ano_G, total_loss
+    def update(self,val , n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum/self.count
+
+def weights_init(module):
+    class_name = module.__class__.__name__
+    if 'Conv' in class_name:
+        module.weight.data.normal_(mean = 0.0, std=0.02)
+    elif 'BatchNorm' in class_name:
+        module.weight.data.normal_(mean=1.0,std=0.02)
+        module.bias.data.fill_(0)
+    return
+
+def anomaly_score(G,D,z,x,__lambda__= 0.1):
+
+    # Residual Loss
+    G_z = G(z)
+    residual_loss = torch.sum(torch.abs(x - G_z))
+
+    # Discrimination Loss
+    feature_G_z , _ = D(G_z)
+    feature_x , _ = D(x)
+    discrimination_loss = torch.sum(torch.abs(feature_x - feature_G_z))
+
+    total_loss = (1 - __lambda__) * residual_loss + __lambda__ * discrimination_loss
+    return G_z, total_loss
+
+def load_data(train_dir, tranform, config):
+    return torch.utils.data.DataLoader(datasets.)
 
